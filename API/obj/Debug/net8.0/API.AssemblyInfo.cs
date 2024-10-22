890b6053b8db3d77f6a633d55ac46f6b694e5fed
Alex <velengura.alex@gmail.com>
2024-10-22 21:07:02 +0300

    Created AppUser Class

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee65cef62f5810f190b9c0f389fdd732b197970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f448d5ce89ce358795a8c0bbd33f325dceeb4374")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
