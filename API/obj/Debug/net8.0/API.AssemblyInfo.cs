11338ffabd1416191b83d7235ae77e394cc63bb7
Alex <velengura.alex@gmail.com>
2024-11-05 21:23:37 +0200

    Created connection string, added nugget dotnet ef

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41fb1133a013837cbc5120e8b72ff74860293e18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9576286e3caa65347faeb4efac4b30db453f314")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
