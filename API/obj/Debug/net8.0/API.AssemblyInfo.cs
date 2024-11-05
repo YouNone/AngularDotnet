2aca40f09cd1bd3a1fbc84eaf5da4af77646492f
Alex <velengura.alex@gmail.com>
2024-11-05 22:48:32 +0200

    change to async

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa5d455a64548ea2dff4b6b2f49d073362a52c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83bb51b6cc38eb572182ffc30f344b10ee26197a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
