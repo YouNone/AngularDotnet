4e30ffe934cefa917c324f3e70566a782b41d090
Alex <velengura.alex@gmail.com>
2024-12-02 22:56:08 +0200

    Added ACcountController with register endpoint + hashing

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dba08063b7ffd8bce98315329cec5575773067d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3e140c0646e37dd71fe8157d6838160a2579cfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
