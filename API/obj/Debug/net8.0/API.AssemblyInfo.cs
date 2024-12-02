f3e140c0646e37dd71fe8157d6838160a2579cfd
Alex <velengura.alex@gmail.com>
2024-12-02 22:32:52 +0200

    Aded  baseApiController

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a846e5d95d20875f2efa9f49ee15cd237f0ac8d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dba08063b7ffd8bce98315329cec5575773067d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
