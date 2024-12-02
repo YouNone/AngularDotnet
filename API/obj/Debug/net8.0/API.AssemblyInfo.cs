a846e5d95d20875f2efa9f49ee15cd237f0ac8d5
Alex <velengura.alex@gmail.com>
2024-12-02 21:11:50 +0200

    adjusted node version

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48716cc4e9b01fc06a0e9363e87001690e25c288")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0810a1d4d8e679b9c8385437be8df6b837279933")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
