dd3c7b84485d25c19e8764a58cccb035b4316929
Alex <velengura.alex@gmail.com>
2025-01-02 21:56:14 +0200

    Added CreateToken functionality in T.Service

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd8d48797dc518302dcd9f8d2ab606d178c5441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa03069f1c434d96fceba99ebb452bed5710f55d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
