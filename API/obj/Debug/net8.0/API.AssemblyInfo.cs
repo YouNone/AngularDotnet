d616d9c514286f8c4043554892dc1f3306e4aad9
Alex <velengura.alex@gmail.com>
2025-01-08 21:27:04 +0200

    account service basic added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed89b103d1c5063061d6991eb52a51ab935e35c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c0908f39e835b1ae9db3ebaa2c9aba3b37fcd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
