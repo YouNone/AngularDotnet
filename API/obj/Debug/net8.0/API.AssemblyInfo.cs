27c0908f39e835b1ae9db3ebaa2c9aba3b37fcd0
Alex <velengura.alex@gmail.com>
2025-01-08 21:16:50 +0200

    created AccountService

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99de7a861a14c6c147cf416e4cfb97f8781ef62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed89b103d1c5063061d6991eb52a51ab935e35c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
