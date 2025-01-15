dbdeb4fffb1e880404495b16299b091e3da46a31
Alex <velengura.alex@gmail.com>
2025-01-15 22:36:58 +0200

    register markup

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7509688ae0c1c6924d1bc7de2fd3532f4626f923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a6a4a373d7be8a56b6cf51d52f1eee553007aed")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
