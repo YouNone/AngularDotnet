3193a9ff2baf1603b3d6a19091b6cf597f50dd14
Alex <velengura.alex@gmail.com>
2025-01-16 23:13:15 +0200

    added routing, blank components

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbdeb4fffb1e880404495b16299b091e3da46a31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259ffe2d556930785136bc3fe6225589914e2f26")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,21 +3,24 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AccountService } from '../services/account.service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
+        this.router.navigateByUrl("/members");
         console.log(response);
       },
       error: error => {
@@ -28,5 +31,6 @@ export class NavComponent {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
