27252b0006988bacf7180bd44e700368f9944f44
Alex <velengura.alex@gmail.com>
2025-01-30 20:51:00 +0200

    Cleared home comp. members list added

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+
+import { MembersService } from '../../services/members.service';
+import { Member } from '../../models/member';
 
 @Component({
   selector: 'app-member-list',
@@ -7,6 +10,19 @@ import { Component } from '@angular/core';
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.scss'
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  
+  private membersService = inject(MembersService);
+  members: Member[] = [];
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersService.getMembers().subscribe({
+      next: members => this.members = members
+    });
+  }
 
 }
