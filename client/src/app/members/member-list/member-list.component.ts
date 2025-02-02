9df1b65ad90b07c11dd6334cc3aa6b2165aca2bb
Alex <velengura.alex@gmail.com>
2025-02-03 00:28:17 +0200

    using members service to store state

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 
 import { MembersService } from '../../services/members.service';
-import { Member } from '../../models/member';
 import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
@@ -13,17 +12,16 @@ import { MemberCardComponent } from "../member-card/member-card.component";
 })
 export class MemberListComponent implements OnInit {
   
-  private membersService = inject(MembersService);
-  members: Member[] = [];
+  membersService = inject(MembersService);
 
   ngOnInit(): void {
-    this.loadMembers();
+    if (this.membersService.members().length === 0) {
+        this.loadMembers();
+    }
   }
 
   loadMembers() {
-    this.membersService.getMembers().subscribe({
-      next: members => this.members = members
-    });
+    this.membersService.getMembers();
   }
 
 }
--- client/src/app/services/members.service.ts
+++ client/src/app/services/members.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { environment } from '../../environments/environment';
 import { Member } from '../models/member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,16 +11,28 @@ import { Member } from '../models/member';
 export class MembersService {
   private http = inject(HttpClient);
   BaseUrl = environment.apiUrl;
+  members = signal<Member[]>([]);
 
   getMembers() {
-    return this.http.get<Member[]>(this.BaseUrl + 'users');
+    return this.http.get<Member[]>(this.BaseUrl + 'users').subscribe({
+        next: members => this.members.set(members)
+    });
   }
 
   getMember(username: string) {
+    const member = this.members().find(mem => mem.username === username);
+    if (member !== undefined) return of(member);
+    
     return this.http.get<Member>(this.BaseUrl + 'users/' + username);
   }
 
   updateMember(member: Member ) {
-    return this.http.put<Member>(this.BaseUrl + 'users', member);
+    return this.http.put<Member>(this.BaseUrl + 'users', member).pipe(
+      tap(() => {
+        this.members
+            .update(membs => membs
+                .map(m => m.username === member.username ? member : m));
+      })  
+    );
   }
 }
