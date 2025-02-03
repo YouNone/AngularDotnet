ebcbd8d08a6b5b1a3d2bbd15e428f23b9b0be8a5
Alex <velengura.alex@gmail.com>
2025-02-03 21:30:02 +0200

    Created PhotoService + interface, added CloudinarySettings in appsettings

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd7c4a61932b64c0cf4dbe1aad43cb3c569ecf6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df1b65ad90b07c11dd6334cc3aa6b2165aca2bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
