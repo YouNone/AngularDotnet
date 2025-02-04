4193e54d85f009f499b2c8670131be0d810a7353
Alex <velengura.alex@gmail.com>
2025-02-04 21:30:06 +0200

    register form rework + custom validator

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+960a2e068f19b2e4436a2e77cd871913817a1460")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e068daeeed27ef0c1430808baae34850be1f05c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
