42d1e13939f88570b09b6f45cc3108a06a769d27
Alex <velengura.alex@gmail.com>
2025-02-05 00:00:33 +0200

    Expanded register form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit, output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 
 import { AccountService } from '../services/account.service';
 import { ToastrService } from 'ngx-toastr';
@@ -16,6 +16,7 @@ import { TextInputComponent } from "../forms/text-input/text-input.component";
 export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService);
   private toastr = inject(ToastrService);
+  private fb = inject(FormBuilder);
   cancelRegister = output<boolean>();
   model: any = {};
   registerForm: FormGroup = new FormGroup({});
@@ -25,10 +26,15 @@ export class RegisterComponent implements OnInit {
   }
 
   initializeForm() {
-    this.registerForm = new FormGroup({
-        username: new FormControl('', Validators.required),
-        password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-        confirmPassword: new FormControl('', [Validators.required, this.matchValues("password")]),
+    this.registerForm = this.fb.group({
+        gender: ['male'],
+        username: ['', Validators.required],
+        knownAs: ['', Validators.required],
+        dateOfBirth: ['', Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+        confirmPassword: ['', [Validators.required, this.matchValues("password")]],
     });
     this.registerForm.controls['password'].valueChanges.subscribe({
         next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
