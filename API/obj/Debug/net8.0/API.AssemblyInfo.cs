385f37705cfd2d4eeecef33c1d76898c3d7b1afb
Alex <velengura.alex@gmail.com>
2025-02-10 20:17:57 +0200

    Api gender + age filter added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bccbbee5128ceb7f1fde127ab0fa61af10751dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6218232d1a7b646052d1b442a1f1747552b2883a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
