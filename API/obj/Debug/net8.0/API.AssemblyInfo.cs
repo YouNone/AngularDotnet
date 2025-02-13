c081ed648db434d2eecbbec172fc800a877c54e1
Alex <velengura.alex@gmail.com>
2025-02-13 20:04:44 +0200

    List component added basic liking works

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd2e3676cde0e714c368059da8d94e143ddd7be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e76065e7851fa2cc31cabafbef0d5623a7226843")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LikesService } from '../services/likes.service';
+import { Member } from '../models/member';
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ButtonsModule, MemberCardComponent],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.scss'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit {
+    private likeService = inject(LikesService);
+    members: Member[] = [];
+    predicate = 'Liked';
+    
+    ngOnInit(): void {
+        this.loadLikes();
+    }
+
+    getTitle() {
+        switch (this.predicate) {
+            case 'liked': return 'Members you like';
+            case 'likedBy': return 'Members who like you';
+            default: return 'Mutual';
+        }
+    }
+
+    loadLikes() {
+        this.likeService.getLikes(this.predicate).subscribe({
+            next: members => this.members = members
+        });
+    }
 
 }
