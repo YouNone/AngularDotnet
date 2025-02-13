71f7a0d5bd5e9df3313728880350d765b1deb4fe
Alex <velengura.alex@gmail.com>
2025-02-13 21:39:33 +0200

    Created paginattion helper, created list pagination

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,21 +1,24 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { LikesService } from '../services/likes.service';
-import { Member } from '../models/member';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+
+import { LikesService } from '../services/likes.service';
 import { MemberCardComponent } from "../members/member-card/member-card.component";
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [FormsModule, ButtonsModule, MemberCardComponent],
+  imports: [FormsModule, ButtonsModule, MemberCardComponent, PaginationModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.scss'
 })
-export class ListsComponent implements OnInit {
-    private likeService = inject(LikesService);
-    members: Member[] = [];
+export class ListsComponent implements OnInit, OnDestroy {
+    
+    likeService = inject(LikesService);
     predicate = 'Liked';
+    pageNumber = 1;
+    pageSize = 5;
     
     ngOnInit(): void {
         this.loadLikes();
@@ -30,9 +33,20 @@ export class ListsComponent implements OnInit {
     }
 
     loadLikes() {
-        this.likeService.getLikes(this.predicate).subscribe({
-            next: members => this.members = members
-        });
+        this.likeService.getLikes(this.predicate, this.pageNumber, this.pageSize);
+    }
+
+    pageChanged(event: any) {
+        if (this.pageNumber !== event.page) {
+            this.pageNumber = event.page;
+            this.loadLikes();
+        }
+    }
+
+    // If I go to messages and then I go back to the lists, then we can see that signal was emptied
+    // in case user get 'liked' when he redirected to see current likes.
+    ngOnDestroy() {
+        this.likeService.paginatedResult.set(null);
     }
 
 }
